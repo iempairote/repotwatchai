dca97ecada20072d174169a36ab7eed804aa4806
iempairote <iempairote@gmail.com>
2012-10-03 14:06:08 +0700

    Two pictures 54160328

---
+++ se.buu.a54160328.MainActivity/gen/se/buu/a54160328/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package se.buu.a54160328;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ se.buu.a54160328.MainActivity/src/se/buu/a54160328/Menu.java
@@ -0,0 +1,5 @@
+package se.buu.a54160328;
+
+public class Menu {
+
+}
